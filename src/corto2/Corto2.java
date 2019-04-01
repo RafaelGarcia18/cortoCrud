1f15de8b8d84293b4d0fa7ae812a247bf78bf224
Rafael Garcia <regestra64@gmail.com>
2019-04-01 14:01:19 -0600

    init

---
+++ src/corto2/Corto2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package corto2;
+
+/**
+ *
+ * @author rafael
+ */
+public class Corto2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
